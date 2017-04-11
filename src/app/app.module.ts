ae0a9a6060f38d91d003de5d72c5d02c8ba789c8
Sander Meijerman <s.meijerman.git@outlook.com>
2017-04-11 11:18:29 +0200

    Move nav to header and remove about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import { ClarityModule } from 'clarity-angular';
 import { AppComponent } from './app.component';
 import { ROUTING } from './app.routing';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        AboutComponent,
         HomeComponent
     ],
     imports: [
