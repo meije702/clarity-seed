70ddbd578c4fd410ca2ace9ee5e82e0101673ae2
Sander Meijerman <s.meijerman.git@outlook.com>
2017-04-28 01:30:06 +0200

    Master detail with detail component

--- src/app/_modules/masterdata/_services/potato-dynamicdata.service.ts
+++ src/app/_modules/masterdata/_services/potato-dynamicdata.service.ts
@@ -1,3 +1,5 @@
+import { forEach } from '@angular/router/src/utils/collection';
+import { Plot } from '../_models/plot';
 import { FARMS } from './mock-farm-masterdata';
 import { PotatoSample } from '../_models/potato-sample';
 import { Injectable } from '@angular/core';
@@ -27,6 +29,22 @@ export class PotatoDynamicdataService {
 
   getPotatoSamples(): PotatoSample[] {
     return this.allPotatoSamples;
+  }
+
+  getPotatoSamplesForPlot(plot: Plot): PotatoSample[] {
+    return this.allPotatoSamples.filter(sample => sample.plot.id === plot.id);
+  }
+
+  getAvgPotatoCountFromSamples(samples: PotatoSample[]): number {
+    let potatoCount = 0;
+    let photoCount = 0;
+    for (let sample of samples) {
+      photoCount += sample.photos.length;
+      for (let photo of sample.photos) {
+        potatoCount += photo.potatoes.length;
+      }
+    }
+    return photoCount > 0 ? potatoCount / photoCount : 0;
   }
 
 }
--- src/app/_modules/report/solgrader-plot-detail/solgrader-plot-detail.component.ts
+++ src/app/_modules/report/solgrader-plot-detail/solgrader-plot-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { PotatoSample } from '../../masterdata/_models/potato-sample';
+import { PotatoDynamicdataService } from '../../masterdata/_services/potato-dynamicdata.service';
+import { Plot } from '../../masterdata/_models/plot';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-solgrader-plot-detail',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class SolgraderPlotDetailComponent implements OnInit {
+    @Input() plot: Plot;
+    PotatoSamples: PotatoSample[];
+    loading: boolean;
+    avgPotatoCount: number;
 
-  constructor() { }
+    constructor(private potatoData: PotatoDynamicdataService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.loading = true;
+        this.PotatoSamples = this.potatoData.getPotatoSamplesForPlot(this.plot);
+        this.avgPotatoCount = this.potatoData.getAvgPotatoCountFromSamples(this.PotatoSamples);
+        this.loading = false;
+    }
 
 }
