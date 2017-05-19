b741b2bb6f389e60441e14ee40621ae2b67e15c1
Sander Meijerman <s.meijerman.git@outlook.com>
2017-05-19 22:23:45 +0200

    working version with latest package versions

--- src/app/_modules/report/solgrader-farm-detail/solgrader-farm-detail.component.ts
+++ src/app/_modules/report/solgrader-farm-detail/solgrader-farm-detail.component.ts
@@ -12,6 +12,50 @@ export class SolgraderFarmDetailComponent implements OnInit {
   public loading: boolean;
   public sizeCatagories: Array<any> = ['0/28', '28/35', '35/50', '50/55', '55/+'];
 
+    // lineChart
+    public lineChartData: Array<any> = [
+        { data: [5, 10, 20, 10, 5, 0, 0], label: 'Yield Bintje 0/28' },
+        { data: [0, 5, 10, 50, 10, 0, 5], label: 'Yield Bintje 28/55' },
+        { data: [0, 0, 5, 10, 20, 10, 5], label: 'Yield Bintje 35/50' },
+        { data: [0, 0, 0, 5, 15, 38, 40], label: 'Yield Bintje 50/55' },
+        { data: [0, 0, 0, 10, 40, 58, 60], label: 'Yield Bintje 55/+' }
+    ];
+
+    public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartOptions: any = {
+        responsive: false
+    };
+
+    public lineChartColors: Array<any> = [
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        },
+        { // dark grey
+            backgroundColor: 'rgba(77,83,96,0.2)',
+            borderColor: 'rgba(77,83,96,1)',
+            pointBackgroundColor: 'rgba(77,83,96,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(77,83,96,1)'
+        },
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+    ];
+    public lineChartLegend = false;
+    public lineChartType = 'line';
+
+
   constructor(private potatoData: PotatoDynamicdataService) { }
 
     ngOnInit() {
--- src/app/_modules/report/solgrader-potato-detail/solgrader-potato-detail.component.ts
+++ src/app/_modules/report/solgrader-potato-detail/solgrader-potato-detail.component.ts
@@ -9,6 +9,45 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SolgraderPotatoDetailComponent implements OnInit {
  @Input() potato: Potato;
 
+    // lineChart
+    public lineChartData: Array<any> = [
+        { data: [23, 30, 40, 50, 55, 58, 60], label: 'Yield' }
+    ];
+
+    public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    public lineChartOptions: any = {
+        responsive: false
+    };
+
+    public lineChartColors: Array<any> = [
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        },
+        { // dark grey
+            backgroundColor: 'rgba(77,83,96,0.2)',
+            borderColor: 'rgba(77,83,96,1)',
+            pointBackgroundColor: 'rgba(77,83,96,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(77,83,96,1)'
+        },
+        { // grey
+            backgroundColor: 'rgba(148,159,177,0.2)',
+            borderColor: 'rgba(148,159,177,1)',
+            pointBackgroundColor: 'rgba(148,159,177,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+        }
+    ];
+    public lineChartLegend = false;
+    public lineChartType = 'line';
+
   constructor() { }
 
   ngOnInit() {
